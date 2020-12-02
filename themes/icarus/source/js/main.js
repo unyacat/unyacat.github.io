6b246b4e1d21e6fdc89da5cccb49ff3d46445fb9
unyacat <admin@unyacat.net>
2020-12-02 13:56:39 +0900

    emoji replace twemoji

--- themes/icarus/source/js/main.js
+++ themes/icarus/source/js/main.js
@@ -1,13 +1,13 @@
 (function ($) {
-    $('.article img:not(".not-gallery-item")').each(function () {
-        // wrap images with link and add caption if possible
-        if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
-            if (this.alt) {
-                $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
-            }
-        }
-    });
+//    $('.article img:not(".not-gallery-item")').each(function () {
+//        // wrap images with link and add caption if possible
+//        if ($(this).parent('a').length === 0) {
+//            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+//            if (this.alt) {
+//                $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
+//            }
+//        }
+//    });
 
     if (typeof (moment) === 'function') {
         $('.article-meta time').each(function () {
